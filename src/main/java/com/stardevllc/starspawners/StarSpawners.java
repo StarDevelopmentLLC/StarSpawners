0ad684e57a69ba8efc4e5dbffb86d992a1b99816
Firestar311 <slaterandrew0398@gmail.com>
2024-10-24 08:46:25 -0500

    Added a pickup mode and a name setting to the config

--- src/main/java/com/stardevllc/starspawners/StarSpawners.java
+++ src/main/java/com/stardevllc/starspawners/StarSpawners.java
@@ -19,7 +19,11 @@ public class StarSpawners extends JavaPlugin {
     @Override
     public void onEnable() {
         this.mainConfig = new Config(new File(getDataFolder(), "config.yml"));
-
+        this.mainConfig.addDefault("spawner.name", "&d{ENTITYNAME} Spawner", " The name for the spawner item", " The only applies to spawners that are picked up via silk touch", "You don't need the entity name in the display name. The plugin tracks the items using NBT Tags");
+//        this.mainConfig.addDefault("spawner.unique", false, "This setting makes it so that all spawners that are created are unique", "This makes them unstackable and makes sure that they cannot be duped");
+        
+        this.mainConfig.addDefault("spawner.pickupmode", "DROP", " Controls the mode in how spawner items are handled when picked up using Silk Touch", " DROP is where the item is dropped on the ground where the spawner was located", " INVENTORY is where the item is given to the player that broke the block directly", " It is recommended on larger servers to change this to INVENTORY to help with item lag");
+        
         if (NMSVersion.CURRENT_VERSION.ordinal() <= NMSVersion.v1_8_R3.ordinal()) {
             playerHandWrapper = new PlayerHandWrapper_1_8();
         } else {
